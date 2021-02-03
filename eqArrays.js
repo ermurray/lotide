b50f3bcd8cf40a4ebfdab6773f310c8c80e187d7
ermurray <ermurray31@gmail.com>
2021-02-03 01:39:44 +0000

    added logic to evaluate if two arrays are equal

--- eqArrays.js
+++ eqArrays.js
@@ -5,3 +5,23 @@ const assertEqual = function(actual, expected) {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let pass = false;
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] === arrayTwo[i]) {
+        pass = true;
+      } else {
+        pass = false;
+      }
+    }
+  }
+  return pass;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
