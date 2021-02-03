036903237998a690cfe4cb988e6c53e03e3e281a
ermurray <ermurray31@gmail.com>
2021-02-03 23:05:06 +0000

    intial add of findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
