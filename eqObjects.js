ac1b3b4fc3cb9e0fff77eadd64e418e7d2832e83
ermurray <ermurray31@gmail.com>
2021-02-04 02:31:32 +0000

    added logic to check if objects were the same if they contained primitives and called the eqArray function inside eqObject to check if the k/v pair was array and equal.

--- eqObjects.js
+++ eqObjects.js
@@ -6,12 +6,45 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arrayOne, arrayTwo) {
+  let pass = true;
+  if (arrayOne.length !== arrayTwo.length) {
+    pass = false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      pass = false;
+    }
+  }
+  return pass;
+};
+
 const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (const key in object1) {
+    if (Array.isArray(object1[key])) {
+      return eqArrays(object1[key], object2[key]);
+    }
+    if (object1[key] !== object2[key]) {
+      
+      return false;
+    }
+  }
+  return true;
 };
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba));
+assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc));
+assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
