6c761b1ce098550570039cd616f6fae2c5b269ca
ermurray <ermurray31@gmail.com>
2021-02-04 20:33:49 +0000

    added map.js

---
+++ map.js
@@ -0,0 +1,8 @@
+const words = ["ground", "control", "to", "major", "tom", "loving", "LHL"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const map = function() {
+
+};
