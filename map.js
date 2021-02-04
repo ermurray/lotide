2c4995587c8e066d0483603bd92d790c5c1d58ae
ermurray <ermurray31@gmail.com>
2021-02-04 21:00:08 +0000

    added assertArraysEqual and tested custom Map function

--- map.js
+++ map.js
@@ -11,3 +11,27 @@ const map = function(array, callback) {
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let pass = true;
+  if (arrayOne.length !== arrayTwo.length) {
+    pass = false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      pass = false;
+    }
+  }
+  return pass;
+};
+
+// eslint-disable-next-line no-unused-vars
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅  Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+const wordsUppercase =["GROUND", "CONTROL", "TO", "MAJOR", "TOM", "LOVING", "LHL"];
+assertArraysEqual(map(words, word => word.toUpperCase()), wordsUppercase);
