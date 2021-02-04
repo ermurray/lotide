dffb0e7e84fac016ae96e32939e975eaece94a93
ermurray <ermurray31@gmail.com>
2021-02-04 00:01:23 +0000

    finished logic and some manual tests

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,3 +5,25 @@ const assertEqual = function(actual, expected) {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const findKeyByValue = function(obj,valToFind) {
+  for (const key in obj) {
+    if (obj[key] === valToFind) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  // eslint-disable-next-line camelcase
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, 123), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, 123), undefined);
+
