9513061d21f5d91a3a12a7252701277383d0e503
ermurray <ermurray31@gmail.com>
2021-02-08 23:21:38 +0000

    middle.js to  have test code in middle.test.js and added assertArraysEqual.js requirements for middle.test

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let pass = true;
-  if (arrayOne.length !== arrayTwo.length) {
-    pass = false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      pass = false;
-    }
-  }
-  return pass;
-};
-
-// eslint-disable-next-line no-unused-vars
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅  Assertion Passed: ${arrayOne} === ${arrayTwo}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
-  }
-};
-
 const middle = function(array) {
   const midIndex = Math.floor(array.length / 2);
   let midArray = [];
@@ -35,9 +13,4 @@ const middle = function(array) {
   return midArray;
 };
 
-//tests
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
