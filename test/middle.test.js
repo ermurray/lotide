94472599e484d7081ebe683a8eca7857e5a08017
ermurray <ermurray31@gmail.com>
2021-02-09 00:15:35 +0000

    refactor of middle.test to use M & C for testing

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,9 +1,28 @@
+const assert = require('chai').assert;
 const middle = require('../middle.js');
-const assertArraysEqual = require('../assertArraysEqual.js');
 
-//tests
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe('#middle', () => {
+
+  it('expect a return of [2] when input is [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('expect a return of [3] when input is [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it('expect a return of [] when input is [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  
+  it('expect a return of [] when input is [\'a\']', () => {
+    assert.deepEqual(middle(['a']), []);
+  });
+  
+  it('expect a return of [] when input is [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it('expect a return of [] when input is [\'b\',\'a\']', () => {
+    assert.deepEqual(middle(['b', 'a']), []);
+  });
+
+});
